67d3e08adcee766e32de718a4fcc5197b799cc4f
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2023-11-26 18:57:28 +0600

    verify token set

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function run() {
     const reviewCollection = client.db("bistrodb").collection("reviews");
     const cartCollection = client.db("bistrodb").collection("carts");
     const paymentCollection = client.db("bistrodb").collection("payments");
-    
+
     // jwt related api
     app.post('/jwt', async (req, res) => {
       const user = req.body;
@@ -44,6 +44,37 @@ async function run() {
       });
       res.send({ token });
     })
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+
+      })
+
+    }
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      next();
+
+    }
 
     // user related api
     app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
