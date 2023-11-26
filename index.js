8bee197ba5697b3779b945db7a2edfc5fbd964e8
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2023-11-26 20:02:33 +0600

    make reviews api

--- index.js
+++ index.js
@@ -176,6 +176,12 @@ async function run() {
       const result = await menuCollection.deleteOne(query)
       res.send(result);
     })
+
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
